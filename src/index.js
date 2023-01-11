dcf4684058de83ce310efe9da79bd17a93677f99
Jakub <malinowski.jakub@yahoo.com>
2023-01-10 19:26:31 -0600

    Javascript and HTML additions and bugs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': API_Key,
-		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-	}
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': API_KEY,
+    'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+  }
 };
 
-fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+searchButton.addEventListener("click", (event) => {
+	event.preventDefault();
+	const searchInput = document.getElementById("search-input").value;
+	// Make the HTTP request to the movie database API
+	fetch(`https://online-movie-database.p.rapidapi.com/auto-complete?q=${searchInput}`, options)
+	  .then(response => response.json())
+	  .then(data => {
+		const searchResults = document.getElementById("search-results");
+		searchResults.innerHTML = "";
+		data.d(movie => {
+		  const movieTitle = document.createElement("p");
+		  movieTitle.innerHTML = movie.Title;
+		  searchResults.appendChild(movieTitle);
+		});
+	  })
+	  .catch(error => console.error(error));
+  });
+  
