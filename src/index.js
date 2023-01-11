fc68a7cca8b8f1ab9c8180346e45070d6c4ae944
Jakub <malinowski.jakub@yahoo.com>
2023-01-10 21:18:54 -0600

    Movies displaying on the screen

--- src/index.js
+++ src/index.js
@@ -5,21 +5,40 @@ const options = {
 		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
 	}
 };
+
 const form = document.querySelector('form');
 const resultsDiv = document.querySelector('#results');
 const searchButton = document.getElementById("search-button");
 const input = document.getElementById("search-input"); 
-
-  searchButton.addEventListener("click", (event) => {
-	  event.preventDefault();
-	  const searchTerm = input.value; 
-	
-	  // Make the HTTP request to the movie database API
-	  fetch(`https://online-movie-database.p.rapidapi.com/auto-complete?q=${searchTerm}`, options)
-	  .then(response => response.json())
-	  .then(response => console.log(response))
-	  .catch(err => console.error(err));
-
-
+searchButton.addEventListener("click", (event) => {
+	event.preventDefault();
+	const searchTerm = input.value; 
+  
+	// Make the HTTP request to the movie database API
+	fetch(`https://online-movie-database.p.rapidapi.com/auto-complete?q=${searchTerm}`, options)
+	.then(response => {
+	  return response.json();
+	})
+	.then(data => {
+	  console.log(data);
+	  const list = data.d;
+  
+	  const movies = Array.prototype.map.call(list, item => {
+		// Check if item.i exists before trying to access its imageUrl property
+		if (item.i) {
+		  const name = item.l;
+		  const poster = item.i.imageUrl;
+		  const movie = `<li>
+			<img src="${poster}">
+			<h2>${name}</h2>
+		  </li>`
+		  return movie;
+		}
+	  });
+  
+	  // Display the movies in the HTML
+	  const moviesList = document.querySelector('.movies');
+	  moviesList.innerHTML = movies.join('');
+	});
   });
   
