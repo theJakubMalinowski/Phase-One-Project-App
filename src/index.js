ba156eceb19397977cd5d0bc7cfe0596eb1d8e2a
Jakub <malinowski.jakub@yahoo.com>
2023-01-10 20:11:08 -0600

    deletion of code, in order to test API fetch return

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const options = {
 	method: 'GET',
 	headers: {
-	  'X-RapidAPI-Key': API_Key,
-	  'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+		'X-RapidAPI-Key': API_URL,
+		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
 	}
-  };
-  
+};
+
+
   const searchButton = document.getElementById("search-button");
   const input = document.getElementById("search-input"); // added this line
   searchButton.addEventListener("click", (event) => {
@@ -14,17 +15,11 @@ const options = {
 	
 	  // Make the HTTP request to the movie database API
 	  fetch(`https://online-movie-database.p.rapidapi.com/auto-complete?q=${searchTerm}`, options)
-		.then(response => response.json())
-		.then(data => {
-		  const searchResults = document.getElementById("search-results");
-		  searchResults.innerHTML = "";
-		  const list = data.d
-		  data.d(movie => {
-			const movieTitle = document.createElement("p");
-			movieTitle.innerHTML = movie.Title;
-			searchResults.appendChild(movieTitle);
-		  });
-		})
+	  .then(response => response.json())
+	  .then(response => console.log(response))
+	  .catch(err => console.error(err));
+
+		console.log(data)
 		.catch(error => console.error(error));
 	});
   
