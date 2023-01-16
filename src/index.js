0904801da854ddac8bc15ed98362e0950f232998
Jakub <malinowski.jakub@yahoo.com>
2023-01-16 01:53:17 -0600

    Perhaps some changes to keep or delete

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': API_URL,
-		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+	  'X-RapidAPI-Key': API_URL,
+	  'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
 	}
-};
-
-const form = document.querySelector('form');
-const resultsDiv = document.querySelector('#results');
-const searchButton = document.getElementById("search-button");
-const input = document.getElementById("search-input"); 
-const toggleBtn = document.querySelector('#toggle-btn');
-
-
-// create a reference to saved movies container
-const savedMoviesContainer = document.querySelector('.saved-movies');
-
-searchButton.addEventListener("click", (event) => {
+  };
+  
+  const form = document.querySelector('form');
+  const resultsDiv = document.querySelector('#results');
+  const searchButton = document.getElementById("search-button");
+  const input = document.getElementById("search-input"); 
+  const toggleBtn = document.querySelector('#toggle-btn');
+  
+  // create a reference to saved movies container
+  const savedMoviesContainer = document.querySelector('.saved-movies');
+  
+  searchButton.addEventListener("click", (event) => {
 	event.preventDefault();
 	const searchTerm = input.value; 
   
@@ -46,26 +45,25 @@ searchButton.addEventListener("click", (event) => {
 	  // Display the movies in the HTML
 	  const moviesList = document.querySelector('.movies');
 	  moviesList.innerHTML = movies.join('');
-      
-      // add event listener to like button
-      const likeButtons = document.querySelectorAll('.like-button');
-      likeButtons.forEach(button => {
-        button.addEventListener('click', (event) => {
-          event.target.innerHTML = 'Liked';
-          event.target.disabled = true;
+  
+	  // add event listener to like button
+	  const likeButtons = document.querySelectorAll('.like-button');
+	  likeButtons.forEach(button => {
+		button.addEventListener('click', (event) => {
+		  event.target.innerHTML = 'Liked';
+		  event.target.disabled = true;
+  
+		  // add the movie to the saved movies container
+		  const closestLi = event.target.closest('li');
+		  if(closestLi) {
+			const movie = closestLi.cloneNode(true); // clone the movie so it stays in the results
+			savedMoviesContainer.appendChild(movie);
+		  } else {
 
-   		 // add the movie to the saved movies container
-		const movie = event.target.closest('li').cloneNode(true); // clone the movie so it stays in the results
-		savedMoviesContainer.appendChild(movie);
-
-		const savedMovie = document.createElement('div');
-		savedMovie.classList.add("movie-item");
-		savedMovie.appendChild(event.target.closest('.movie-item').cloneNode(true));
-		const savedMovies = document.querySelector('.saved-movies');
-		savedMovies.appendChild(savedMovie);
-
-  		});
-      });
+		  }
+  
+		});
+	  });
 	});
   });
 
