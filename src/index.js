45a20a77bce7d5999427356fb5fe40aed57d1001
Jakub <malinowski.jakub@yahoo.com>
2023-01-31 19:47:02 -0600

    restructuring

--- src/index.js
+++ src/index.js
@@ -4,62 +4,62 @@ const options = {
 	  'X-RapidAPI-Key': API_URL,
 	  'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
 	}
-};
-
-const searchButton = document.getElementById("search-button");
-const input = document.getElementById("search-input"); 
-const toggleBtn = document.querySelector('#toggle-btn');
-const savedMoviesContainer = document.querySelector('.saved-movies');
-
-searchButton.addEventListener("click", (event) => {
+  };
+  
+  const searchButton = document.getElementById("search-button");
+  const input = document.getElementById("search-input"); 
+  const toggleBtn = document.getElementById('toggle-btn');
+  const savedMoviesContainer = document.querySelector('.saved-movies');
+  
+  // Function to generate movie element
+  function generateMovieElement(poster, name) {
+	const movie = document.createElement('li');
+	
+	const moviePoster = document.createElement('img');
+	moviePoster.src = poster;
+	movie.appendChild(moviePoster);
+	
+	const movieName = document.createElement('h2');
+	movieName.textContent = name;
+	movie.appendChild(movieName);
+	
+	const likeButton = document.createElement('button');
+	likeButton.classList.add('like-button');
+	likeButton.textContent = 'Like';
+	likeButton.addEventListener('click', (event) => {
+	  event.target.innerHTML = 'Liked';
+	  event.target.disabled = true;
+	  savedMoviesContainer.appendChild(movie.cloneNode(true));
+	});
+	movie.appendChild(likeButton);
+  
+	return movie;
+  }
+  
+  searchButton.addEventListener("click", (event) => {
 	event.preventDefault();
 	const searchTerm = input.value; 
-
+  
 	fetch(`https://online-movie-database.p.rapidapi.com/auto-complete?q=${searchTerm}`, options)
-	.then(response => response.json())
-	.then(data => {
-	  console.log(data);
-	  const list = data.d;
-  
-	  const movies = Array.prototype.map.call(list, item => {
-		if (item.i) {
-		  const name = item.l;
-		  const poster = item.i.imageUrl;
-		  const movie = `<li>
-			<img src="${poster}">
-			<h2>${name}</h2>
-			<button class="like-button">Like</button>
-		  </li>`
-		  return movie;
-		}
-	  });
-	  const moviesList = document.querySelector('.movies');
-	  moviesList.innerHTML = movies.join('');
-
-	  const likeButtons = document.querySelectorAll('.like-button');
-	  likeButtons.forEach(button => {
-		button.addEventListener('click', (event) => {
-		  event.target.innerHTML = 'Liked';
-		  event.target.disabled = true;
-  
-		  const closestLi = event.target.closest('li');
-		  if(closestLi) {
-			const movie = closestLi.cloneNode(true); 
-			savedMoviesContainer.appendChild(movie);
+	  .then(response => response.json())
+	  .then(data => {
+		const listOfMovies = data.d;
+		const moviesList = document.querySelector('.movies');
+		moviesList.innerHTML = '';
+	
+		listOfMovies.forEach(movie => {
+		  if (movie.i) {
+			const movieElement = generateMovieElement(movie.i.imageUrl, movie.l);
+			moviesList.appendChild(movieElement);
 		  }
 		});
 	  });
-	});
-});
+  });
+  
   // Add a click event listener to the toggle button
   toggleBtn.addEventListener('click', () => {
 	// Toggle the visibility of the saved movies element
 	const savedMoviesElement = document.querySelector('.saved-movies');
 	savedMoviesElement.classList.toggle('hidden');
-
-  if (savedMoviesElement.style.display === 'none') {
-	savedMoviesElement.style.display = 'block';
-  } else {
-	savedMoviesElement.style.display = 'none';
-  }
-});
+  });
+  
